350032cb0f88bb3f4e802282b15f09a9004ac854
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2025-08-01 18:01:23 -0700

    1.1.1

--- AutomaticRematch/Main.cs
+++ AutomaticRematch/Main.cs
@@ -14,6 +14,7 @@ namespace AutomaticRematch
         private Mod AutomaticRematch = new Mod();
         private bool enabled = true;
         private float timeToWait = 0f;
+        private int buttonToUse = 0;
 
         private void Log(string msg)
         {
@@ -23,7 +24,7 @@ namespace AutomaticRematch
         public static class BuildInfo
         {
             public const string ModName = "AutomaticRematch";
-            public const string ModVersion = "1.0.0";
+            public const string ModVersion = "1.1.1";
             public const string Description = "Automatically Presses Rematch Button if On";
             public const string Author = "UlvakSkillz";
             public const string Company = "";
@@ -42,26 +43,23 @@ namespace AutomaticRematch
             AutomaticRematch.SetFolder("AutomaticRematch");
             AutomaticRematch.AddToList("Auto Rematch", true, 0, "Automatically Presses Rematch Button if On", new Tags { });
             AutomaticRematch.AddToList("Time before Pressing", 0f, "Time to wait before it Presses Rematch", new Tags { });
+            AutomaticRematch.AddToList("Which Button To Use", 0, $"Sets What Button it Watches to Prevent Automatic Rematching{Environment.NewLine}0 : Left Controller Trigger{Environment.NewLine}1 : Left Controller Primary{Environment.NewLine}2 : Left Controller Secondary{Environment.NewLine}3 : Right Controller Trigger{Environment.NewLine}4 : Right Controller Primary{Environment.NewLine}5 : Right Controller Secondary", new Tags { });
+            AutomaticRematch.GetFromFile();
             AutomaticRematch.ModSaved += Save;
+            Save();
             UI.instance.AddMod(AutomaticRematch);
         }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             currentScene = sceneName;
-            //if ((currentScene == "Map0") || (currentScene == "Map1")) { MelonCoroutines.Start(Test()); }
         }
-
-        /*private IEnumerator Test()
-        {
-            yield return new WaitForSeconds(5);
-            MelonCoroutines.Start(MatchEnded());
-        }*/
 
         private void Save()
         {
             enabled = (bool)AutomaticRematch.Settings[0].SavedValue;
             timeToWait = (float)AutomaticRematch.Settings[1].SavedValue;
+            buttonToUse = (int)AutomaticRematch.Settings[2].SavedValue;
         }
 
         private void matchEnded()
@@ -98,7 +96,34 @@ namespace AutomaticRematch
                 }
             }
             yield return new WaitForSeconds(timeToWait);
-            myButton.GetComponent<RematchButton>().RPC_OnToggleStateChanged(true);
+            float buttonValue = 0;
+            switch (buttonToUse)
+            {
+                case 0:
+                    buttonValue = Calls.ControllerMap.LeftController.GetTrigger();
+                    break;
+                case 1:
+                    buttonValue = Calls.ControllerMap.LeftController.GetPrimary();
+                    break;
+                case 2:
+                    buttonValue = Calls.ControllerMap.LeftController.GetSecondary();
+                    break;
+                case 3:
+                    buttonValue = Calls.ControllerMap.RightController.GetTrigger();
+                    break;
+                case 4:
+                    buttonValue = Calls.ControllerMap.RightController.GetPrimary();
+                    break;
+                case 5:
+                    buttonValue = Calls.ControllerMap.RightController.GetSecondary();
+                    break;
+                default:
+                    break;
+            }
+            if (buttonValue < 0.5f)
+            {
+                myButton.GetComponent<RematchButton>().RPC_OnToggleStateChanged(true);
+            }
             yield break;
         }
     }
